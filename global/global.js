ab5c7854c0bc8a6e4ada545545d3f3f6d52b53b4
lilBunnyRabbit <andraz_ms@hotmail.com>
2021-06-30 01:08:33 +0200

    Updates | Image Split 2.0.0

--- global/global.js
+++ global/global.js
@@ -1,75 +1,115 @@
 function round(value, deicmals = 2) {
-    const p = Math.pow(10, deicmals < 1 ? 1 : deicmals);
-    return Math.round(value * p) / p;
+  const p = Math.pow(10, deicmals < 1 ? 1 : deicmals);
+  return Math.round(value * p) / p;
 }
 
 function downloadCanvas(input, filename, extra) {
-    if(!input || !Array.isArray(input)) return;
-    createAlert("Downloading", `Downloading ${input.length} images as "${filename}.zip"`);
+  if (!input || !Array.isArray(input)) return;
+  createAlert(
+    "Downloading",
+    `Downloading ${input.length} images as "${filename}.zip"`
+  );
 
-    const zip = new JSZip();
-    input.forEach((canvas, i) => {
-        const data = canvas.toDataURL("image/png").replace("data:image/png;base64,", "");
-        return zip.file(`${filename}_${i + 1}.png`, data, { base64: true });
-    });
+  const zip = new JSZip();
+  input.forEach((canvas, i) => {
+    const data = canvas
+      .toDataURL("image/png")
+      .replace("data:image/png;base64,", "");
+    return zip.file(`${filename}_${i + 1}.png`, data, { base64: true });
+  });
 
-    if(extra) zip.file(extra.name, extra.data);
+  if (extra) zip.file(extra.name, extra.data);
 
-    return zip.generateAsync({ type: "blob" }).then((blob) => {
-        const link = document.createElement("a");
-        link.target = "_blank";
-        link.href = window.URL.createObjectURL(blob);
-        link.download = filename;
-        link.click();
-        return link.remove();
-    });
+  return zip.generateAsync({ type: "blob" }).then((blob) => {
+    const link = document.createElement("a");
+    link.target = "_blank";
+    link.href = window.URL.createObjectURL(blob);
+    link.download = filename;
+    link.click();
+    return link.remove();
+  });
 }
 
 function createImage(src, callback, options = {}) {
-    const img = new Image();
-    for(const key in options) img[key] = options[key];
-    img.crossOrigin = "anonymous";
-    img.onload = (...props) => callback(img, ...props);
-    img.src = src;
+  const img = new Image();
+  for (const key in options) img[key] = options[key];
+  img.crossOrigin = "anonymous";
+  img.onload = (...props) => callback(img, ...props);
+  img.src = src;
 }
 
 function readFile(file, callback) {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = (e) => callback(e, file, reader);
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = (e) => callback(e, file, reader);
 }
 
 function createCanvas() {
-    const canvas = document.createElement("canvas");
-    return [ canvas, canvas.getContext("2d") ];
+  const canvas = document.createElement("canvas");
+  return [canvas, canvas.getContext("2d")];
 }
 
 function createAlert(title, message) {
-    const parent = document.getElementById("alert-box");
-    const alert = document.createElement("div");
-    alert.classList.add("alert");
-    parent.prepend(alert);
+  const parent = document.getElementById("alert-box");
+  const alert = document.createElement("div");
+  alert.classList.add("alert");
+  parent.prepend(alert);
 
-    const alertTitle = document.createElement("div");
-    alertTitle.classList.add("alert-title");
-    alertTitle.innerText = title;
-    alert.appendChild(alertTitle);
+  const alertTitle = document.createElement("div");
+  alertTitle.classList.add("alert-title");
+  alertTitle.innerText = title;
+  alert.appendChild(alertTitle);
 
-    const alertDescription = document.createElement("div");
-    alertDescription.classList.add("alert-description");
-    alertDescription.innerText = message;
-    alert.appendChild(alertDescription);
+  const alertDescription = document.createElement("div");
+  alertDescription.classList.add("alert-description");
+  alertDescription.innerText = message;
+  alert.appendChild(alertDescription);
 
-    return setInterval(() => alert.remove(), 4000);
+  return setInterval(() => alert.remove(), 4000);
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function deleteAllCookies() {
+  let cookies = document.cookie.split(";");
+
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i];
+    const eqPos = cookie.indexOf("=");
+    const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  }
 }
 
 class HandlerBase {
-    constructor() {}
+  constructor() {}
 
-    addListeners(events) {
-        for(const event of events) {
-            document.getElementById(event[0])
-                    .addEventListener(event[1], event[2].bind(this));
-        }
+  addListeners(events) {
+    for (const event of events) {
+      document
+        .getElementById(event[0])
+        .addEventListener(event[1], event[2].bind(this));
     }
+  }
 }
--- routes/image-split/index.js
+++ routes/image-split/index.js
@@ -1,118 +1,303 @@
-window.onload = () => new class extends HandlerBase {
-    constructor() {
-        super();
-        this.canvases = [];
-        this.url = "";
-        this.multiplier = 1;
-        this.image = undefined;
-        this.isHorizontal = false;
-        this.columns = 1;
-        this.rows = 1;
+window.onload = () => {
+  ["--primary", "--secondary", "--bg", "--alert"].forEach((name) => {
+    const color = getCookie("color" + name);
+    if (color) {
+      document.querySelector(":root").style.setProperty(name, color);
+      document.getElementById(`${name.substr(2)}-color`).value = color;
+    }
+  });
 
-        this.addListeners([
-            [ "initial-file-upload", "change", this.handleInitialFileUpload ],
-            [ "file-upload", "change", this.handleFileUpload ],
-            [ "multiplier", "input", this.handleSlider ],
-            [ "toggle-orientation", "click", this.handleOrientationToggle ],
-            [ "primary-color", "input", (e) => this.handleColorChange(e.target.value, "--primary") ],
-            [ "download-button", "click", this.handleDownload ]
-        ]);
+  document.getElementById("upload-page").style.display = "flex";
+
+  new (class extends HandlerBase {
+    constructor() {
+      super();
+      this.url = "";
+      this.multiplier = 1;
+      this.image = undefined;
+      this.isHorizontal = false;
+      this.columns = 1;
+      this.rows = 1;
+      this.size = 0;
+
+      this.addListeners([
+        ["initial-file-upload", "change", this.handleInitialFileUpload],
+        ["file-upload", "change", this.handleFileUpload],
+        ["multiplier", "input", this.handleSlider],
+        ["toggle-direction", "click", this.handleOrientationToggle],
+        [
+          "primary-color",
+          "input",
+          (e) => this.handleColorChange(e.target.value, "--primary"),
+        ],
+        [
+          "secondary-color",
+          "input",
+          (e) => this.handleColorChange(e.target.value, "--secondary"),
+        ],
+        [
+          "bg-color",
+          "input",
+          (e) => this.handleColorChange(e.target.value, "--bg"),
+        ],
+        [
+          "alert-color",
+          "input",
+          (e) => this.handleColorChange(e.target.value, "--alert"),
+        ],
+        ["download-button", "click", this.handleDownload],
+        ["download-back-button", "click", this.handleReturnFromDownload],
+        ["reset-colors", "click", this.handleResetColors],
+      ]);
     }
 
+    // Handlers
     handleInitialFileUpload(e) {
-        document.getElementById("initial").remove();
-        return this.handleFileUpload(e);
+      document.getElementById("upload-page").style.display = "none";
+      document.getElementById("main-page").style.display = "flex";
+      return this.handleFileUpload(e);
     }
 
     handleFileUpload(e) {
-        const input = e.target;
+      const input = e.target;
 
-        if (!input.files || !input.files[0]) return;
-    
-        return readFile(input.files[0], (e, file) => {
-            this.url = e.target.result;
-    
-            document.getElementById("filename").value = file.name.split('.').slice(0, -1).join('.');
+      if (!input.files || !input.files[0]) return;
 
-            createImage.bind(this)(this.url, (image) => {
-                this.image = image;
-                this.isHorizontal = this.image.width >= this.image.height;
-                Array.from(document.getElementsByClassName("hide")).forEach(element => element.classList.remove("hide"));
-                createAlert("Uploading", `${file.name} is being uploaded`);
-                this.updateCanvas();
-            }, { crossOrigin: "anonymous" })
-        });
+      return readFile(input.files[0], (e, file) => {
+        this.url = e.target.result;
+
+        document.getElementById("filename").value = file.name
+          .split(".")
+          .slice(0, -1)
+          .join(".");
+
+        createImage.bind(this)(
+          this.url,
+          (image) => {
+            this.image = image;
+            this.isHorizontal = this.image.width >= this.image.height;
+            this.setToggleOrientationButton();
+            createAlert("Uploading", `${file.name} is being uploaded`);
+            this.updateCanvas();
+          },
+          { crossOrigin: "anonymous" }
+        );
+      });
     }
-    
+
     handleSlider() {
-        this.multiplier = document.getElementById("multiplier").value;
-        return this.updateCanvas();
+      this.multiplier = document.getElementById("multiplier").value;
+      return this.updateCanvas();
     }
 
     handleOrientationToggle() {
-        this.isHorizontal = !this.isHorizontal;
-        return this.updateCanvas();
+      if (!this.image) return;
+      this.isHorizontal = !this.isHorizontal;
+      this.setToggleOrientationButton();
+      return this.updateCanvas();
     }
 
     handleColorChange(color, variable) {
-        return document.querySelector(":root").style.setProperty(variable, color);
+      setCookie("color" + variable, color, 365 * 5);
+      return document.querySelector(":root").style.setProperty(variable, color);
     }
 
     handleDownload() {
-        const filename = document.getElementById("filename").value;
-        return downloadCanvas(this.canvases, filename, { 
-            name: "example.txt", 
-            data: this.createEmojiData(filename) 
-        });
+      if (!this.image) return;
+      document.getElementById("main-page").style.display = "none";
+      document.getElementById("download-page").style.display = "flex";
+      return setTimeout(() => this.downloadSplitCanvas(), 100);
+    }
+
+    handleReturnFromDownload() {
+      document.getElementById("main-page").style.display = "flex";
+      document.getElementById("download-page").style.display = "none";
+
+      document.getElementById("download-list").textContent = "";
+      document.getElementById("download-bar").style.width = "0%";
+
+      document.getElementById("download-back-button").style.display = "none";
+
+      Array.from(document.getElementsByClassName("download-link")).forEach(
+        (link) => link.remove()
+      );
+    }
+
+    handleResetColors() {
+      [
+        ["--primary", "#90D627"],
+        ["--secondary", "#23272a"],
+        ["--bg", "#2C2F33"],
+        ["--alert", "#D63611"],
+      ].forEach((color) => {
+        setCookie("color" + color[0], color[1], -1);
+        document.querySelector(":root").style.setProperty(color[0], color[1]);
+        document.getElementById(`${color[0].substr(2)}-color`).value = color[1];
+      });
+    }
+
+    // Logic
+    setToggleOrientationButton() {
+      document.getElementById("toggle-direction").innerHTML = `Set to ${
+        this.isHorizontal ? "vertical" : "horizontal"
+      }`;
     }
 
     createEmojiData(filename) {
-        let data = "";
-        for (let i = 0; i < this.rows; i++) {
-            for (let j = 0; j < this.columns; j++) {
-                data += `:${filename}_${i * this.columns + j + 1}:`
-            }      
-            if(i < this.rows) data += "\n";
+      let data = "";
+      for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.columns; j++) {
+          data += `:${filename}_${i * this.columns + j + 1}:`;
         }
-        return data;
+        if (i < this.rows) data += "\n";
+      }
+      return data;
     }
 
     updateCanvas() {
-        const container = document.getElementById("canvas-container");
-        container.innerHTML = "";
-        this.canvases = [];
-    
-        const size = (this.isHorizontal ? this.image.height : this.image.width) / this.multiplier;
-        const ratio = (((window.innerWidth + window.innerHeight) / 2) / ((this.image.width + this.image.height) / 2)) / 2.5;
-        const canvas_size = size * ratio;
+      if (this.isHorizontal) {
+        this.columns = Number.parseInt(this.multiplier);
+        this.size = this.image.width / this.columns;
+        this.rows = Math.ceil(this.image.height / this.size);
+      } else {
+        this.rows = Number.parseInt(this.multiplier);
+        this.size = this.image.height / this.rows;
+        this.columns = Math.ceil(this.image.width / this.size);
+      }
 
-        const x_multiplier = Math.ceil(this.image.width / size);
-        const y_multiplier = Math.ceil(this.image.height / size);
+      const canvas = document.getElementById("canvas");
+      const ctx = canvas.getContext("2d");
 
-        this.columns = x_multiplier;
-        this.rows = y_multiplier;
+      let space = this.size / 10;
+      if (space < 5) space = 5;
+      else if (space > 15) space = 15;
 
-        container.style.gridTemplateColumns = "auto ".repeat(x_multiplier);
-        container.style.gridTemplateRows = "auto ".repeat(y_multiplier);
+      canvas.width = this.columns * this.size + (this.columns - 1) * space;
+      canvas.height = this.rows * this.size + (this.rows - 1) * space;
 
-        for (let y = 0; y < y_multiplier; y++) {
-            for (let x = 0; x < x_multiplier; x++) {
-                const [ canvas, ctx ] = createCanvas();
-
-                canvas.width = canvas_size;
-                canvas.height = canvas_size;
-                canvas.style.border = "inherit";
-
-                ctx.drawImage(this.image, x * size, y * size, size, size, 0, 0, canvas.width, canvas.height);
-
-                container.appendChild(canvas);  
-                this.canvases.push(canvas);         
-            }                
+      for (let r = 0; r < this.rows; r++) {
+        for (let c = 0; c < this.columns; c++) {
+          ctx.drawImage(
+            this.image,
+            c * this.size,
+            r * this.size,
+            this.size,
+            this.size,
+            c * (this.size + space),
+            r * (this.size + space),
+            this.size,
+            this.size
+          );
         }
+      }
 
-        document.getElementById("ratio").innerText = `${x_multiplier}x${y_multiplier}`;
-        document.getElementById("square-size").innerText = `${round(canvas_size, 2)}px`;
-        document.getElementById("download-display").innerText = `Download ${this.canvases.length} images`;
+      document.getElementById(
+        "ratio"
+      ).innerText = `${this.columns}x${this.rows}`;
+      document.getElementById("square-size").innerText = `${round(
+        this.size,
+        2
+      )}px`;
+      document.getElementById("download-display").innerText = `Download ${
+        this.columns * this.rows
+      } image${this.columns * this.rows === 1 ? "" : "s"}`;
     }
-}
 
+    addToDownladList(element) {
+      const downloadList = document.getElementById("download-list");
+
+      if (typeof element === "string") {
+        const fileP = document.createElement("p");
+        fileP.innerText = element;
+        downloadList.appendChild(fileP);
+      } else {
+        downloadList.appendChild(element);
+      }
+
+      downloadList.scrollTop = downloadList.scrollHeight;
+    }
+
+    updateDownloadBar(percent) {
+      const downloadBar = document.getElementById("download-bar");
+      downloadBar.style.width = `${percent}%`;
+      downloadBar.innerText = `${Math.round(percent)}%`;
+    }
+
+    downloadSplitCanvas() {
+      const filename = document.getElementById("filename").value;
+
+      const [canvas, ctx] = createCanvas();
+
+      canvas.width = this.size;
+      canvas.height = this.size;
+
+      let totalFiles = this.rows * this.columns + 1;
+
+      const zip = new JSZip();
+
+      for (let r = 0; r < this.rows; r++) {
+        for (let c = 0; c < this.columns; c++) {
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+          ctx.drawImage(
+            this.image,
+            c * this.size,
+            r * this.size,
+            this.size,
+            this.size,
+            0,
+            0,
+            this.size,
+            this.size
+          );
+
+          const fileNumber = r * this.columns + c + 1;
+          const fullName = `${filename}_${fileNumber}.png`;
+
+          const data = canvas
+            .toDataURL("image/png")
+            .replace("data:image/png;base64,", "");
+          zip.file(fullName, data, { base64: true });
+
+          this.addToDownladList(`Created ${fullName}`);
+          this.updateDownloadBar((fileNumber * 50) / totalFiles);
+        }
+      }
+
+      zip.file("example.txt", this.createEmojiData(filename));
+      this.addToDownladList("Created example.txt");
+      this.updateDownloadBar(50);
+
+      this.addToDownladList("Creating zip...");
+
+      let zipDone = false;
+      let lastFile;
+
+      return zip
+        .generateAsync({ type: "blob" }, ({ currentFile, percent }) => {
+          if (currentFile) {
+            if (currentFile != lastFile) {
+              this.addToDownladList(`Zipped ${currentFile}`);
+              lastFile = currentFile;
+            }
+            this.updateDownloadBar(50 + percent / 2);
+          } else if (!zipDone) {
+            this.addToDownladList("Creating download link...");
+            zipDone = true;
+          }
+        })
+        .then((blob) => {
+          const link = document.createElement("a");
+          link.classList = "download-link";
+          link.target = "_blank";
+          link.href = window.URL.createObjectURL(blob);
+          link.download = filename;
+          link.innerText = `${filename}.zip`;
+          link.onclick = this.handleReturnFromDownload;
+          this.addToDownladList(link);
+
+          document.getElementById("download-back-button").style.display =
+            "flex";
+        });
+    }
+  })();
+};
